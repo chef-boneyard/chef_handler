074037f329ad3934df0d6aedef27728f61e20662
Mike Fiedler <miketheman@gmail.com>
2013-01-01 17:22:29 -0500

    [COOK-2146] updated style

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: chef_handlers
 # Attribute:: default
 #
-# Copyright 2011, Opscode, Inc
+# Copyright 2011-2013, Opscode, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,12 +19,12 @@
 #
 
 default["chef_handler"]["root_user"] = "root"
+
 case platform
-when "openbsd","freebsd","mac_os_x","mac_os_x_server"
+when "openbsd", "freebsd", "mac_os_x", "mac_os_x_server"
   default["chef_handler"]["root_group"] = "wheel"
 else
   default["chef_handler"]["root_group"] = "root"
 end
 
-
-default["chef_handler"]["handler_path"] = "#{File.expand_path(File.join(Chef::Config[:file_cache_path],'..'))}/handlers"
+default["chef_handler"]["handler_path"] = "#{File.expand_path(File.join(Chef::Config[:file_cache_path], '..'))}/handlers"

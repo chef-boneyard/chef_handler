851ca7864e027b02547b455d103707fbec608c20
Douglas Thrift <douglas.thrift@rightscale.com>
2014-03-28 14:41:03 -0700

    [COOK-4494] Fix comments

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,9 +1,9 @@
 #
 # Author:: Douglas Thrift (<douglas.thrift@rightscale.com>)
 # Cookbook Name:: chef_handler
-# Libraries:: matchers
+# Library:: matchers
 #
-# Copyright 2014, Opscode, Inc.
+# Copyright 2014, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
